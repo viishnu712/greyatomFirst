f7df130f0018053ecc6c79cd430799dfcebe5e3e
vishnu <viishnu712@gmail.com>
2018-01-13 16:37:20 +0530

    added 3 functions

--- date.py
+++ date.py
@@ -1,1 +1,4 @@
-# code to print date and time
+#a function that will return only current date
+#a function that will retun curent year
+#a function that will return timestamp
+#a function to return current month
